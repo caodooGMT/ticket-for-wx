8d3019e71ee6b6243de8e74c374eec1ed07405f9
silencegmt <496555782@qq.com>
2017-08-21 16:54:23 +0800

    bootstrap datetimepicker

--- js/common.js
+++ js/common.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
-    $.get("../components/header.html", function (data) {
-        $("#header").html(data);
-    });
-});
-
-$(document).ready(function () {
     $.get("../components/footer.html", function (data) {
         $("#footer").html(data);
     });
-});
-
-$(document).ready(function () {
     $.get("../components/main-content.html", function (data) {
         $("#main-content").html(data);
     });
+    $.get("../components/header.html", function (data) {
+        $("#header").html(data);
+    });
+    console.log('000');
+    $('#main-content-date-picker').datetimepicker({
+        format: 'L',
+        locale: 'zh-cn'
+    });
+    console.log('111');
 });
+
